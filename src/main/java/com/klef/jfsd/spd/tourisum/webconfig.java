76cab6bd46f91318c4e19e1718201fd5af177078
MannavaKamal <141606700+MannavaKamal@users.noreply.github.com>
2024-12-11 14:09:48 +0530

    Update webconfig.java

--- src/main/java/com/klef/jfsd/spd/tourisum/webconfig.java
+++ src/main/java/com/klef/jfsd/spd/tourisum/webconfig.java
@@ -9,7 +9,7 @@ public class webconfig implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:3000") // React frontend URL
+	                .allowedOrigins("*") // React frontend URL
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                .allowCredentials(true); // Allow credentials (for cookies)
 	    }
