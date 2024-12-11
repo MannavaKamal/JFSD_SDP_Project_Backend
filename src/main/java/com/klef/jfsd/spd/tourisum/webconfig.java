3ee89825a423d4fad4bdb9b85741e1dd751eb2ec
MannavaKamal <141606700+MannavaKamal@users.noreply.github.com>
2024-12-11 17:33:16 +0530

    Update webconfig.java

--- src/main/java/com/klef/jfsd/spd/tourisum/webconfig.java
+++ src/main/java/com/klef/jfsd/spd/tourisum/webconfig.java
@@ -9,7 +9,9 @@ public class webconfig implements WebMvcConfigurer {
 	 @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOrigins("https://jfsd-sdp-project-frontend-react-2zquuwhpf.vercel.app") // React frontend URL
+	                .allowedOrigins(  "https://jfsd-sdp-project-frontend-react-2zquuwhpf.vercel.app", 
+                    "https://jfsd-sdp-project-frontend-react-git-main-mannavakamals-projects.vercel.app"
+) // React frontend URL
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
 	                .allowCredentials(true); // Allow credentials (for cookies)
 	    }
