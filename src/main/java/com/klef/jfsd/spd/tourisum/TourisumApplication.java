73458cf52f87d6cade34bb0fc02ace01fe801fa2
MannavaKamal <141606700+MannavaKamal@users.noreply.github.com>
2024-12-13 11:17:49 +0530

    Update TourisumApplication.java

--- src/main/java/com/klef/jfsd/spd/tourisum/TourisumApplication.java
+++ src/main/java/com/klef/jfsd/spd/tourisum/TourisumApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableRedisHttpSession	
+@EnableRedisHttpSession	
 public class TourisumApplication {
 
 	public static void main(String[] args) {
