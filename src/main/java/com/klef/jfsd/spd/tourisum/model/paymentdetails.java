52524eb835b902ad19cdaf5207f2e4f9c9f53149
MannavaKamal <141606700+MannavaKamal@users.noreply.github.com>
2024-12-13 12:36:41 +0530

    Update paymentdetails.java

--- src/main/java/com/klef/jfsd/spd/tourisum/model/paymentdetails.java
+++ src/main/java/com/klef/jfsd/spd/tourisum/model/paymentdetails.java
@@ -9,7 +9,8 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="payment_table")
-public class paymentdetails {
+public class paymentdetails implements java.io.Serializable{
+	 private static final long serialVersionUID = 1L;
 	 @Id
 	 @GeneratedValue(strategy = GenerationType.IDENTITY) // you can take this manually also through form
 	 @Column(name="sno")
